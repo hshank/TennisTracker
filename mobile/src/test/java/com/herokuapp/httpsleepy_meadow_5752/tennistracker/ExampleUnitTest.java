64027161ca296cfd826bae9c97e114da4ff47146
Julian <jwongchiu@gmail.com>
2015-11-10 03:28:09 -0800

    FirstCommit

---
+++ mobile/src/test/java/com/herokuapp/httpsleepy_meadow_5752/tennistracker/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.herokuapp.httpsleepy_meadow_5752.tennistracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
